0f03f525596f6a73b905b2c7bbcb1b36ac740888
William Marchesi <55283021+marchesiwatwit@users.noreply.github.com>
2020-11-20 13:33:50 -0500

    Completed Lab 7

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,2 +1,32 @@
+
 public class InsertionSort {
+
+	public static int[] sort(int[] input) {
+		
+		for (int i = 1; i < input.length - 1; i++) {
+			int v = input[i];
+			int j = i-1;
+			
+			while (j >= 0 && input[j] > v) {
+				input[j+1] = input[j];
+				j = j-1;
+			}
+			
+			input[j+1] = v;
+		}
+		
+		return input;
+	}
+	
+	public static void main(String[] args) {
+		int[] toSort = {34, 56, 11, 22, 55, 98, 43, 67};
+		
+		int[] out = sort(toSort);
+		
+		for (int num : out) {
+			System.out.println(num);
+		}
+
+	}
+
 }
